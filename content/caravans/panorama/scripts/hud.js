25c715e1f0b11a4c598b8c5af5ef21ccaa175b00
IGRINI <grigmat78@gmail.com>
2017-10-08 14:22:46 +0300

    Фиксы худа

--- content/caravans/panorama/scripts/hud.js
+++ content/caravans/panorama/scripts/hud.js
@@ -1,6 +1,7 @@
 var selectedspawnpoint = null;
 var hud;
 var minimap;
+var killcam;
 var death = false;
 var timergo = false;
 function UpdatePresents(table, key, data) {
@@ -45,7 +46,7 @@ function FormatTime(seconds)
 }
 function StartRespawnTimer()
 {
-	var time = Players.GetRespawnSeconds(Players.GetLocalPlayer())
+	var time = Players.GetRespawnSeconds(Players.GetLocalPlayer()) + 1
 	if(time <= 5 && selectedspawnpoint == null)
 	{
 		$("#UntilTime").style.position = "0% -15% 0%";
@@ -78,6 +79,7 @@ function StartRespawnTimer()
 		$("#UntilTime").style.color = "white";
 		$("#DeathTimer").style.color = "white";
 	}
+	$.Msg(death)
 }
 function CloseButton() 
 {
@@ -92,78 +94,101 @@ function CloseButton()
 }
 function CloseDeathScreen()
 {
+	death = false;
 	if(Game.IsHUDFlipped())
 	{
 		$("#DeathPanel").style.position = "100% 0% 0%";
-		$("#CloseLabel").style.position = "3% 0% 0%";
+		$("#CloseLabel").style.position = "2.5% 0% 0%";
 		$("#CloseLabel").style.transform = "rotateZ(90deg)";
 	}
 	else
 	{
 		$("#DeathPanel").style.position = "-100% 0% 0%";
-		$("#CloseLabel").style.position = "-3% 0% 0%";
+		$("#CloseLabel").style.position = "-2.5% 0% 0%";
 		$("#CloseLabel").style.transform = "rotateZ(-90deg)";
 	}
 	$("#CloseLabel").text = $.Localize("#Open");
 	$("#CloseLabel").style.visibility = "collapse";
-	minimap.style.position = "0% 0% 0%";
 	for (var i = 1; i <= 4; i++) 
 	{
 		$("#spawnpoint_" + i).style["wash-color"] = 'gray';
 	}
 	selectedspawnpoint = null;
-	death = false;
 }
 function CloseDS() 
 {
+	death = false;
 	if(Game.IsHUDFlipped())
 	{
 		$("#DeathPanel").style.position = "100% 0% 0%";
-		$("#CloseLabel").style.position = "3% 0% 0%";
+		$("#CloseLabel").style.position = "2.5% 0% 0%";
 		$("#CloseLabel").style.transform = "rotateZ(90deg)";
 	}
 	else
 	{
 		$("#DeathPanel").style.position = "-100% 0% 0%";
-		$("#CloseLabel").style.position = "-3% 0% 0%";
+		$("#CloseLabel").style.position = "-2.5% 0% 0%";
 		$("#CloseLabel").style.transform = "rotateZ(-90deg)";
 	}
 	$("#CloseLabel").style.visibility = "visible";
 	$("#CloseLabel").text = $.Localize("#Open");
-	minimap.style.position = "0% 0% 0%";
-	death = false;
 }
 function ShowDeathScreen()
 {
 	death = true;
 	$("#DeathPanel").style.position = "0% 0% 0%";
 	$("#CloseLabel").style.visibility = "visible";
-	if(Game.IsHUDFlipped())
-	{
-		$("#CloseLabel").style.position = "-7.8% 24% 0%";
-	}
-	else
-	{
-		$("#CloseLabel").style.position = "7.8% 24% 0%";
-	}
 	$("#CloseLabel").text = $.Localize("#Close");
 	$("#CloseLabel").style.transform = "rotateZ(0deg)";
-	var pos = -(minimap.FindChildTraverse("minimap").actualuiscale_x * minimap.FindChildTraverse("minimap").desiredlayoutwidth);
+	if(timergo == false)
+	{
+		timergo = true
+		$.Schedule(0.2, function(){StartRespawnTimer();});
+	}
+	HideMiniMapAndKillCam();
+}
+function HideMiniMapAndKillCam()
+{
+	var pos = -250;
 	if(Game.IsHUDFlipped())
 	{
 		pos = -pos
 	}
 	minimap.style.position = pos + "px 0% 0%";
-	if(timergo == false)
+	killcam.style.transform = "translateX("+ pos * 2 +"px)";
+	if(Game.IsHUDFlipped())
 	{
-		timergo = true
-		$.Schedule(0.2, function(){StartRespawnTimer();});
+		$("#CloseLabel").style.position = "-7.2% 24% 0%";
+	}
+	else
+	{
+		$("#CloseLabel").style.position = "7.2% 24% 0%";
+	}
+	if(death == true)
+	{
+		$.Schedule(0.01, function(){HideMiniMapAndKillCam();});
+	}
+	else
+	{
+		minimap.style.position = "0% 0% 0%";
+		killcam.style.transform = "translateX(0px)";
+		if(Game.IsHUDFlipped())
+		{
+			$("#CloseLabel").style.position = "2.5% 0% 0%";
+			$("#CloseLabel").style.transform = "rotateZ(90deg)";
+		}
+		else
+		{
+			$("#CloseLabel").style.position = "-2.5% 0% 0%";
+			$("#CloseLabel").style.transform = "rotateZ(-90deg)";
+		}
 	}
 }
 (function()
 {
 	hud = $.GetContextPanel().GetParent().GetParent().GetParent();
 	minimap = hud.FindChildTraverse("HUDElements").FindChildTraverse("minimap_container");
+	killcam = hud.FindChildTraverse("HUDElements").FindChildTraverse("KillCam");
 	minimap.style["transition-property"] = "position;"
 	minimap.style["transition-duration"] = "0.2s;"
 	minimap.style["transition-timing-function"] = "ease-in;"
